44d394b78b8c3bb8b74962596935e8ce96581164
Samantha Ngo <sngo1@stuy.edu>
2017-10-04 13:16:35 -0400

    more work

--- 10.4.17/app.py
+++ 10.4.17/app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, session
 app = Flask(__name__) # create instance of class
 
-# assign following fxn to run when root route requested
-@app.route("/")
-def login():
-    print "200: Login page loaded"
-    return render_template('login.html')
+# Naming the session
+session["Jeff"] = ""
+session["username"] = ""
+session["password"] = ""
 
-#@app.route("/auth")
-#def names():
-#    render_template('runMe.html',name=form["username"].value)
+# Initially loading the webpage
+@app.route("/", methods=["POST"])
+def login():
+    if session["username"] != "":
+        return render_template('welcome.html')
+    elif session["username"] == "":
+        print "200: Login page loaded"
+        return render_template('login.html')
+
+@app.route("/auth")
+def authentification():
+    # Get user-inputted username and password
+    username = request.args["username"]
+    password = request.args["password"]
+
+    if username == "
+    render_template('runMe.html',name=form["username"].value)
     
 if __name__ == "__main__":
     app.debug = True

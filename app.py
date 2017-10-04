96cff20e4b092fa8ebedda9a0efe12ab7c85d013
Helen Ye <00yhelen@gmail.com>
2017-10-04 18:46:27 -0400

    Update templates and app

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 from flask import Flask, render_template, request, session
+import os
 app = Flask(__name__) # create instance of class
 
-session["jeff"] = ""
+username = "jeff_sessions123"
 password = "Super secret password"
+
+app.secret_key = os.urandom(32)
 
 # Initially loading the webpage, load the welcome page
 # if they're logged in
-@app.route("/", methods=["POST"])
+@app.route("/")
 def login():
-    if session.get("jeff") == "jeff_sessions123":
-        return render_template('welcome.html')
+    if session.get("jeff") == username:
+        return render_template('welcome.html',
+                               username = username)
     else:
-        return render_template('login.html')
+        return render_template('login.html',
+                               msg = "")
 
-@app.route("/auth")
+@app.route("/auth", methods=["POST"])
 def authentification():
     # Get user-inputted username and password
     userIn = request.form["username"]
     passIn = request.form["password"]
     if userIn == username and passIn == password:
         session["jeff"] = userIn
-        return render_template("welcome.html")
+        return render_template("welcome.html",
+                               username = username)
     else:
         return render_template("login.html",
-                               error_msg = "Incorrect user or pass")
+                               msg = "Incorrect user or pass")
+
+@app.route("/logout")
+def logout():
+    session["jeff"] = ""
+    return render_template("login.html",
+                            msg = "Logged out!")
 
 if __name__ == "__main__":
     app.debug = True

a86ef53213c5326e72e44030d7ef373d89e54656
sngo1 <sngo1@stuy.edu>
2017-10-04 22:05:38 -0400

    added comments

--- app.py
+++ app.py
@@ -1,39 +1,62 @@
+# Team Turbo Funiculator
+# Samantha Ngo, Helen Ye, Shaina Peters
+# SoftDev -- pd7
+# hw07
+# 2017-10-04
+
 from flask import Flask, render_template, request, session
 import os
-app = Flask(__name__) # create instance of class
 
+# Create instance of class
+app = Flask(__name__) 
+
+# Hard-coded username and password
 username = "jeff_sessions123"
 password = "Super secret password"
 
+# Generate secret key to make cookies immutable to others.
 app.secret_key = os.urandom(32)
 
-# Initially loading the webpage, load the welcome page
-# if they're logged in
+# If initially loading the webpage, load the login page.
+# If they're already logged in, then load the welcome page.
 @app.route("/")
 def login():
-    if session.get("jeff") == username:
+    print "Session.get('Jeff') returns ", session.get("Jeff")
+    if session.get("Jeff") == username:
         return render_template('welcome.html',
                                username = username)
     else:
         return render_template('login.html',
                                msg = "")
 
+# Authentification Process
 @app.route("/auth", methods=["POST"])
 def authentification():
-    # Get user-inputted username and password
+    # Get user-inputted username and password from the form
     userIn = request.form["username"]
+    print "U: " + userIn
     passIn = request.form["password"]
+    print "P: " + passIn
+
+    # If username and password are correct... 
     if userIn == username and passIn == password:
-        session["jeff"] = userIn
+        # ...open a new session and...
+        session["Jeff"] = userIn
+        # ...render the welcome page.
         return render_template("welcome.html",
-                               username = username)
+                               username = userIn)
+    # Otherwise, render the login page(again) with an error message.
     else:
         return render_template("login.html",
-                               msg = "Incorrect user or pass")
+                               msg = "Incorrect username or password. Nice try, Jeff Sessions. ")
 
+# Logging out renders the login page.
+# Should it reroute to the login page instead?
 @app.route("/logout")
 def logout():
-    session["jeff"] = ""
+    # End the session
+    session["Jeff"] = ""
+    # Render Login Page
     return render_template("login.html",
                             msg = "Logged out!")
 
--- auth/runMe.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-import cgitb
-
-form = cgi.FieldStorage()
-render_template('runMe.html',name=form["username"].value)
-render_template('runMe.html',name=form["username"].value)
